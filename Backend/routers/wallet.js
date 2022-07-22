24c20262facd1f0594469ffd722c084c861f65ba
Keith Shepherdson <kmson7@gmail.com>
2022-07-22 15:33:17 +0800

    save before pulling changes

--- Backend/routers/wallet.js
+++ Backend/routers/wallet.js
@@ -2,10 +2,33 @@ const express = require('express')
 const router = express.Router()
 const Wallet = require('../models/walletModel')
 
+// router.get('/', (req,res) => {
+//     Wallet.find().then(wallets => {
+//         res.json(wallets);
+//     })
+// })
+
+
 router.get('/', (req,res) => {
-    Wallet.find().then(wallets => {
+    // Wallet.find().then(wallets => {
+    //     res.json(wallets);
+    // })
+    Wallet.find({ user_id: 1 }).then(wallets => {
         res.json(wallets);
     })
+    
+    
 })
+
+
+
+
+// const query = Wallet.findOne({ 'user_id': '1' });
+// console.log(query)
+
+// query.exec(function (err, person) {
+//     if (err) return handleError(err);
+//     console.log(person.user_id)
+//   });
 
 module.exports = router
