8bc175ac7fe6e2d848a7957868168f9f10c13aad
Xing Da <xingda@u.nus.edu>
2022-07-22 11:26:09 +0800

    update walletModel and create utils folder

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -17,6 +17,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-})
+},
+    {
+        timestamps: true
+    }
+
+)
 
 module.exports = mongoose.model('User', UserSchema)
---
+++ Backend/models/walletModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const WalletSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+    
+},
+    {
+        timestamps: true
+    }
+
+)
+
+module.exports = mongoose.model('Wallet', UserSchema)
