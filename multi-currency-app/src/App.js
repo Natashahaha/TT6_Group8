199b8042cfa23220d9c98a507fe3844d2b2a7f6a
oliverlimgj <109592038+oliverlimgj@users.noreply.github.com>
2022-07-22 18:37:45 +0800

    add imports

--- multi-currency-app/src/App.js
+++ multi-currency-app/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Login2 from "./pages/Login2";
+import Register2 from "./pages/Register2";
 import DashBoard from "./pages/DashBoard";
 import ManageWallet from "./pages/ManageWallet";
 
